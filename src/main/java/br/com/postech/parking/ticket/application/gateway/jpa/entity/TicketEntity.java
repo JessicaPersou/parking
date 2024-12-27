cf04bb10ee61bfa73d61eab18080449cb65ae38d
Jessica Sousa <jessica.persou@gmail.com>
2024-12-26 23:55:14 -0300

    relationships with entities

--- src/main/java/br/com/postech/parking/ticket/application/gateway/jpa/entity/TicketEntity.java
+++ src/main/java/br/com/postech/parking/ticket/application/gateway/jpa/entity/TicketEntity.java
@@ -1,11 +1,15 @@
 package br.com.postech.parking.ticket.application.gateway.jpa.entity;
 
 import br.com.postech.parking.ticket.domain.TicketStatusEnum;
+import br.com.postech.parking.user.application.gateway.jpa.entity.UserEntity;
+import br.com.postech.parking.vehicle.application.gateway.jpa.entity.VehicleEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -39,4 +43,12 @@ public class TicketEntity {
 
     @Column(name = "price")
     private BigDecimal price;
+
+    @ManyToOne
+    @JoinColumn(name = "vehicle_id", nullable = false)
+    private VehicleEntity vehicle;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private UserEntity user;
 }
